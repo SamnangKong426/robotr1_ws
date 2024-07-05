425256b380464a307fd3f8c771332953c3208d9c
SamnangKong426 <samnangkong499@gmail.com>
2024-07-05 16:17:56 +0700

    update

--- src/r1/arduino_mega/components.ino
+++ src/r1/arduino_mega/components.ino
@@ -14,11 +14,11 @@ void init_hardware() {
   pinMode(grap, OUTPUT);
   pinMode(grip2, OUTPUT);
   pinMode(EN, OUTPUT);
-  gun.setMaxSpeed(1200);
-  gunStepper.addStepper(gun);
+  // gun.setMaxSpeed(1200);
+  // gunStepper.addStepper(gun);
 
-  // gun.setMaxSpeed(12000);
-  // gun.setAcceleration(12000);
+  gun.setMaxSpeed(1200);
+  gun.setAcceleration(12000);
   // gun.setSpeed(1200);
 
   digitalWrite(grip1, 0);
