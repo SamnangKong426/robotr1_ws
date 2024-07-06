20085890e162222a4a6a786406bd82698cc4a7a7
SamnangKong426 <samnangkong499@gmail.com>
2024-07-06 15:05:53 +0700

    update

--- ws_clone.sh
+++ ws_clone.sh
@@ -6,11 +6,3 @@ rm -rf build/ log/ install/
 
 # colcon build
 # source ~/r1_ws/install/setup.bash
-
-# compile, 
-arduino-cli compile --fqbn arduino:avr:mega src/r1/arduino_mega/
-
-# upload
-arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:mega  src/r1/arduino_mega/
-
-arduino-cli monitor -p /dev/ttyUSB0 --fqbn arduino:avr:mega --config baudrate=115200
